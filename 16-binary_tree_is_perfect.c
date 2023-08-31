cc3e9dc259be8d0c64b6ae8bcca044799125908d
Eric Ubaga <ubaga91@gmail.com>
2023-08-31 13:20:54 +0300

    included the height parameter checks for all levels in checking for perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,26 +18,32 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_full - checks if a binary tree is full
+ * tree_is_perfect - checks if a binary tree is full
  * @tree: pointer to the roor node of the tree
  * Return: 1 if tree is full, otherwise 0
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+int tree_is_perfect(const binary_tree_t *tree)
 {
-	int l_tree = 0, r_tree = 0;
+	int l_tree = 0, r_tree = 0, l_height, r_height;
+
+	if (tree == NULL)
+		return (1);
+
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
 
 	if (tree && ((tree->left && tree->right) ||
 				(!(tree->left) && !(tree->right))))
 	{
 		if (tree->left && tree->right)
 		{
-			l_tree = binary_tree_is_full(tree->left);
-			r_tree = binary_tree_is_full(tree->right);
-			if (l_tree && r_tree)
+			l_tree = tree_is_perfect(tree->left);
+			r_tree = tree_is_perfect(tree->right);
+			if (l_tree && r_tree && (l_height == r_height))
 				return (1);
 			return (0);
 		}
-		else if (!(tree->left) && !(tree->right))
+		else if (!(tree->left) && !(tree->right) && (l_height == r_height))
 			return (1);
 		else
 			return (0);
@@ -53,16 +59,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int l_height = 0, r_height = 0, is_full = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	l_height = binary_tree_height(tree->left);
-	r_height = binary_tree_height(tree->right);
-	is_full = binary_tree_is_full(tree);
-
-	if ((l_height == r_height) && is_full)
-		return (1);
-	return (0);
+	return (tree_is_perfect(tree));
 }
